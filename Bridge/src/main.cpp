6646af6e856724502a1e3e5c6354c41f40f6cf1e
Omik Save <27330809+omiksave@users.noreply.github.com>
2022-08-26 15:33:28 -0700

    Finalizing Insole Wrapper

--- Bridge/src/main.cpp
+++ Bridge/src/main.cpp
@@ -8,23 +8,28 @@ using ip::tcp;
 
 int main() {
 
-	InsoleWrapper I1;
+	InsoleWrapper* I1 = new InsoleWrapper();
 	//InsoleWrapper I2;
-	I1.StartThread();
+	I1->StartThread();
 	//I2.UpdateInsole();
 	boost::asio::io_service io_service;
 	//socket creation
 	tcp::socket socket(io_service);
 	//connection
-	socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("192.168.7.1"), 5001));
+	//socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("192.168.7.1"), 5001));
+
 	 //request/message from client
 	boost::system::error_code error;
 
+	std::atomic<bool> runner = true;
 
-	while(1) {
-		boost::asio::write(socket, boost::asio::buffer(I1.p, 128 * 4), error);
-		std::this_thread::sleep_for(std::chrono::microseconds(100));
-	}
+	//std::thread tthr(sendPacket, pointrunner, I1, error, socket);
+	std::cin.get();
+	runner = false;
+	//while(1) {
+	//	boost::asio::write(socket, boost::asio::buffer(I1.p, 128 * 4), error);
+	//	std::this_thread::sleep_for(std::chrono::microseconds(100));
+	//}
 
 	std::cin.get();
 
