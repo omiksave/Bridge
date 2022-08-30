cc631f3765fb5e8ca098d594f871be28f82eb7f2
Omik Save <27330809+omiksave@users.noreply.github.com>
2022-08-29 20:55:59 -0700

    Developing IMU Wrapper

--- Bridge/src/IMUWrapper.cpp
+++ Bridge/src/IMUWrapper.cpp
@@ -2,10 +2,18 @@
 
 IMUWrapper::IMUWrapper(int offsetx)
 {
-	initSensors(offsetx);
+	offset = offsetx;//Set offset
+	initSensors();//Initialize Sensor
 }
 
-void IMUWrapper::initSensors(int offsetx)
+void IMUWrapper::initSensors()
 {
-	tss_getComPorts(comport,1,offsetx,TSS_FIND_ALL_KNOWN);
+	tss_getComPorts(comport,1,offset,TSS_FIND_ALL_KNOWN);//Get current device COM PORT
+	*device = tss_createTSDeviceStr(comport->com_port, TSS_TIMESTAMP_SENSOR);//Assign device to COM PORT
+	*(color+offset) = 1;//Set unique color
+	*error = tss_setLEDColor(*device, color, timestamp);//Set LED Color to identify
+}
+
+void IMUWrapper::updateIMU()
+{
 }
--- Bridge/src/IMUWrapper.h
+++ Bridge/src/IMUWrapper.h
@@ -11,8 +11,21 @@ class IMUWrapper
 private:
 	TSS_Device_Id* device = new TSS_Device_Id;//Create unique device ID
 	TSS_ComPort* comport = new TSS_ComPort;//Create COM PORT object 
+	int offset = 0;//Default offset
+	TSS_Error* error = new TSS_Error;//Create error instance
+	float* euler = new float[3]{};//Create Euler Angle Buffer
+	float* color = new float[3]{ 0,0,0 };//Create Unique color identifier
+	unsigned int* timestamp = new unsigned int[1]();//Create timestamp variable
+	/********************************************Client Multithreading Member Variables*****************************************/
+	std::mutex blockIMU;//Create lock for pointer when copying data from API
+	std::thread threadIMU;//Create pointer for thread
 public:
-	IMUWrapper(int offsetx);
-	void initSensors(int offsetx);
+	IMUWrapper(int offsetx);//Default constructor
+	void initSensors();//Initialize IMU
+	void updateIMU();//Update Euler Angles
+	void threadIMUFunc();//Loop Update until asked to stop
+	void startIMUThread();//Start Thread
+	void stopIMUThread();//Stop Thread
+	~IMUWrapper(void);//Class destructor
 };
 
