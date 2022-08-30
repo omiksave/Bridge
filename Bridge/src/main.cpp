3c873edf92abc2da34933ad51d677cff4fb25ac1
Omik Save <27330809+omiksave@users.noreply.github.com>
2022-08-30 15:18:55 -0700

    Troubleshooting

--- Bridge/src/main.cpp
+++ Bridge/src/main.cpp
@@ -6,10 +6,13 @@ int main() {
 
 	timeBeginPeriod(1);//Change system granularity to 1 ms, default 10ms
 	TCPCOM_Client MyClient("192.168.7.1", 5001);//Initialize communication with Speedgoat
+	std::cout << "Finished Setup" << std::endl;
+	std::cin.get();
 	MyClient.startClientThread();//Start Client thread
 	std::cin.get();//Press Enter to safely interrupt
 	MyClient.stopClientThread();//Stop Client Thread
 	timeEndPeriod(1);//Return system granularity to default
+	std:: cin.get();
 	return 0;
 }
 
