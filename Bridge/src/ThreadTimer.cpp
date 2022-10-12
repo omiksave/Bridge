daf3d8fa2c075eb7a5ad1ae669c1507c81b1adc3
Omik Save <27330809+omiksave@users.noreply.github.com>
2022-10-12 10:55:04 -0700

    Annote ThreadTimer

--- Bridge/src/ThreadTimer.cpp
+++ Bridge/src/ThreadTimer.cpp
@@ -2,9 +2,9 @@
 
 ThreadTimer::~ThreadTimer()
 {
-	_end = std::chrono::high_resolution_clock::now();
-	_elapsed = (_end - _start).count();
-	if (_elapsed < _delay) {
-		std::this_thread::sleep_for(std::chrono::nanoseconds(_delay - _elapsed));
+	_end = std::chrono::high_resolution_clock::now();// Note end loop execution time
+	_elapsed = (_end - _start).count();// Calculate elapsed time
+	if (_elapsed < _delay) {// Criteria for pause
+		std::this_thread::sleep_for(std::chrono::nanoseconds(_delay - _elapsed));// Pause thread for recommended delay
 	}
 }
--- Bridge/src/ThreadTimer.h
+++ Bridge/src/ThreadTimer.h
@@ -2,16 +2,19 @@
 #include<iostream>
 #include<chrono>
 #include<thread>
-
+/* This is a timer class for thread time management. This class is intended to be used at the begining of an infinite loop (preferably while),
+* where when the object goes out of scope the object calculates the loop execution time, compares with the expected delay and pauses execution
+* for the exact remainder time in nanoseconds.
+*/
 class ThreadTimer
 {
 private:
-	std::chrono::high_resolution_clock::time_point _start = std::chrono::high_resolution_clock::now();
-	std::chrono::high_resolution_clock::time_point _end;
-	long long _elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(_end - _start).count();
-	long long _delay = 500000;
+	std::chrono::high_resolution_clock::time_point _start = std::chrono::high_resolution_clock::now();// Set initial time
+	std::chrono::high_resolution_clock::time_point _end;// Initiate end time variable
+	long long _elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(_end - _start).count();// Initiate duration variable
+	long long _delay = 500000;// Initiate delay variable
 public:
-	ThreadTimer(long long delay) { _delay = delay; };
-	~ThreadTimer();
+	ThreadTimer(long long delay) { _delay = delay; };// Intialize object
+	~ThreadTimer();// Destruct object
 };
 
