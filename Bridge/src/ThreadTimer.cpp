9129bd6c3bde026c50834c58ec4be1d8d4b27b6c
Omik Save <27330809+omiksave@users.noreply.github.com>
2022-10-12 10:50:48 -0700

    Updating ThreadTimer

--- Bridge/src/ThreadTimer.cpp
+++ Bridge/src/ThreadTimer.cpp
@@ -1,1 +1,10 @@
 #include "ThreadTimer.h"
+
+ThreadTimer::~ThreadTimer()
+{
+	_end = std::chrono::high_resolution_clock::now();
+	_elapsed = (_end - _start).count();
+	if (_elapsed < _delay) {
+		std::this_thread::sleep_for(std::chrono::nanoseconds(_delay - _elapsed));
+	}
+}
--- Bridge/src/ThreadTimer.h
+++ Bridge/src/ThreadTimer.h
@@ -5,6 +5,13 @@
 
 class ThreadTimer
 {
-
+private:
+	std::chrono::high_resolution_clock::time_point _start = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point _end;
+	long long _elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(_end - _start).count();
+	long long _delay = 500000;
+public:
+	ThreadTimer(long long delay) { _delay = delay; };
+	~ThreadTimer();
 };
 
